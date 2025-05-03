7d2f2869c36335e37fce74725a7d6f36e7b10c5b
ArielVS <146688229+ArielArmandoVargasSaucedo@users.noreply.github.com>
2025-05-03 03:15:28 -0400

    feat(minio): minio module created for connection with MinioS3

---
+++ src/minio/minio.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { MinioService } from './minio.service';
+import { MinioController } from './minio.controller';
+
+@Module({
+  controllers: [MinioController],
+  providers: [MinioService],
+  exports: [MinioService],
+})
+export class MinioModule {}
