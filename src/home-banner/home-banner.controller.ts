1197e3f614ac7defc6fd036df9f0dc56e83b261c
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-13 10:59:12 -0400

    docs(home-banner-controller): documented home-banner-controller

--- src/home-banner/home-banner.controller.ts
+++ src/home-banner/home-banner.controller.ts
@@ -10,27 +10,81 @@ import {
 import { HomeBannerService } from './home-banner.service';
 import { CreateHomeBannerDto } from './dto/create-home-banner.dto';
 import { UpdateHomeBannerDto } from './dto/update-home-banner.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { HomeBannerResponse } from './responses/home-banner.response';
+import { HomeBannerDetailsResponse } from './responses/home-banner-details.response';
 
 @Controller('home-banner')
 export class HomeBannerController {
   constructor(private readonly homeBannerService: HomeBannerService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Este endpoint agrega un banner del home a la base de datos',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Banner del Home creado exitosamente',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Ocurrió un error en el proceso de creación de la oferta',
+  })
   create(@Body() createHomeBannerDto: CreateHomeBannerDto) {
     return this.homeBannerService.create(createHomeBannerDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary:
+      'Este endpoint obtiene una lista de banners del home de la base de datos',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de banners del home obtenida exitosamente',
+    type: HomeBannerResponse,
+    isArray: true,
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      'Ocurrió un error en el proceso de obtener la lista de banners del home',
+  })
   findAll() {
     return this.homeBannerService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary:
+      'Este endpoint obtiene un banner del home en específico de la base de datos',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Banner del Home obtenida exitosamente',
+    type: HomeBannerDetailsResponse,
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Ocurrió un error en el proceso de obtener el banner del home',
+  })
   findOne(@Param('id') id: string) {
     return this.homeBannerService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Este endpoint edita un banner del home de la base de datos',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Banner del home editado exitosamente',
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      'Ocurrió un error en el proceso de edición del banner del home',
+  })
   update(
     @Param('id') id: string,
     @Body() updateHomeBannerDto: UpdateHomeBannerDto,
@@ -39,6 +93,18 @@ export class HomeBannerController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Este endpoint elimina un banner del home de la base de datos',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Banner del home eliminado exitosamente',
+  })
+  @ApiResponse({
+    status: 500,
+    description:
+      'Ocurrió un error en el proceso de eliminación del banner del home',
+  })
   remove(@Param('id') id: string) {
     return this.homeBannerService.remove(id);
   }
