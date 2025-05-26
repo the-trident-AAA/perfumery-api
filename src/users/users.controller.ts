3ae464f90f43a71d5e5d7dd02bc7851e05694901
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-05-26 00:03:20 -0400

    docs(users): add Swagger documentation for user retrieval endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, Body, Delete, Param, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { UserResponse } from './responses/user.response';
 
 @Controller('users')
 export class UsersController {
@@ -12,6 +14,19 @@ export class UsersController {
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Este endpoint obtiene una lista de usuarios de la base de datos',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de usuarios obtenida exitosamente',
+    type: UserResponse,
+    isArray: true,
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Ocurrió un error en el proceso de obtener lista de usuarios',
+  })
   find() {
     return this.usersService.find();
   }
