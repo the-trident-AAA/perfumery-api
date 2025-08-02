b10d728b2a2c5b89360dd3619ae609f5025eef22
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-02 10:53:39 -0400

    feat(order): add response classes for the order and associated perfumes

---
+++ src/order/responses/order-perfume.response.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+import { PerfumeResponse } from 'src/perfume/responses/perfume.response';
+
+export class OrderPerfumeResponse {
+  @ApiProperty({
+    description: 'Representa el identificador del perfume de la orden',
+    type: 'number',
+  })
+  id: string;
+  @ApiProperty({
+    description: 'Representa el perfume seleccionado para dicha orden',
+    type: PerfumeResponse,
+  })
+  perfume: PerfumeResponse;
+  @ApiProperty({
+    description: 'Representa la cantidad seleccionada del perfume de la orden',
+    type: 'number',
+  })
+  cant: number;
+  @ApiProperty({
+    description:
+      'Representa el precio total (precio del perfume multiplicado por la cantidad)',
+    type: 'number',
+  })
+  @Expose()
+  get price(): number {
+    return this.perfume.price * this.cant;
+  }
+}
---
+++ src/order/responses/order.response.ts
@@ -0,0 +1,51 @@
+import { UserResponse } from 'src/users/responses/user.response';
+import { State } from '../entities/state.enum';
+import { OrderPerfumeResponse } from './order-perfume.response';
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+
+export class OrderResponse {
+  @ApiProperty({
+    description: 'Representa el identificador de la orden',
+    type: 'number',
+  })
+  id: string;
+  @ApiProperty({
+    description: 'Representa el estado de la orden',
+    example: 'por definir',
+  })
+  state: State;
+  @ApiProperty({
+    description: 'Representa el usuario asociado a la orden',
+    type: UserResponse,
+  })
+  user: UserResponse;
+  @ApiProperty({
+    description: 'Representa los perfumes asociados a la orden',
+    type: OrderPerfumeResponse,
+    isArray: true,
+  })
+  orderPerfumes: OrderPerfumeResponse[];
+
+  @ApiProperty({
+    description: 'Cantidad total de ítems del pedido',
+    type: Number,
+  })
+  @Expose()
+  get totalItems(): number {
+    return this.orderPerfumes.reduce((total, orderPerufme) => {
+      return total + orderPerufme.cant;
+    }, 0);
+  }
+
+  @ApiProperty({
+    description: 'Monto total del pedido (suma del precio de todos los ítems)',
+    type: Number,
+  })
+  @Expose()
+  get totalMount(): number {
+    return this.orderPerfumes.reduce((total, orderPerfume) => {
+      return total + orderPerfume.price;
+    }, 0);
+  }
+}
