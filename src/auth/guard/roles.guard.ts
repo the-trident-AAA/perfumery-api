30333fc866e776fdcef73ff82ed9a32f963ffde5
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-02 10:21:44 -0400

    feat(auth): improved role validation logic in the authentication guard

--- src/auth/guard/roles.guard.ts
+++ src/auth/guard/roles.guard.ts
@@ -1,20 +1,27 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { ROLES_KEY } from '../decorators/roles.decorator';
-import { Role } from '../../common/enums/role.enum';
+
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  // Para acceder a los metadatos es necesario usar Reflector
+  constructor(private reflector: Reflector) { }
+  canActivate(
+    context: ExecutionContext,
+  ): boolean {
+    let entrada: boolean = true
+    // Se obtienen los roles definidos en los meta datos del decorador "Roles"
+    const roles: Array<String> = this.reflector.getAllAndOverride("roles", [context.getHandler(),
+    context.getClass()]) // getHandler da una referencia al método del controlador de ruta de esta forma es posible identificar el metadato a extraer sobre que controlador será
+    // getClass hace referencia a la clase del contexto de la llamada al guarda al igual que getHandler que hacer referencia al método del controlador de ruta
+    // si el metadato de los roles está definido
+    if (roles) {
+      // se obtiene el rol del token
+      const { user } = context.switchToHttp().getRequest()
 
-  canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<Role>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-
-    if (!requiredRoles) {
-      return true;
+      // si el rol del token no se encuentra dentro de los roles permitidos para esa ruta
+      if (!roles.includes(user.role))
+        entrada = false
     }
 
     const { user } = context.switchToHttp().getRequest();
