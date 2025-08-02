fc0d0c318f7b3d51d60a754233861b6c30d004e0
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-02 16:33:47 -0400

    feat(order): add Swagger documentation for the Total property in the UserTotalOrdersResponse response.

--- src/order/responses/user-total-orders.respose.ts
+++ src/order/responses/user-total-orders.respose.ts
@@ -1,4 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UserTotalOrdersResponse {
+  @ApiProperty({
+    description: 'Representa el total de pedidos del usuario',
+    type: 'number',
+  })
   total: number;
 
   constructor(total: number) {
