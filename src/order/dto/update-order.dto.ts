102d24c8b273763135f5402878915d91b6e7fe84
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-02 23:36:24 -0400

    refactor(order): remove CreateOrderDto and adjust UpdateOrderDto to include the perfume logic.

--- src/order/dto/update-order.dto.ts
+++ src/order/dto/update-order.dto.ts
@@ -1,3 +1,30 @@
-import { CreateOrderDto } from './create-order.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsEnum, IsUUID, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { State } from '../entities/state.enum';
 
-export class UpdateOrderDto extends CreateOrderDto {}
+class PerfumeOrderDto {
+  @ApiProperty({ description: 'ID del perfume' })
+  @IsUUID()
+  perfumeId: string;
+
+  @ApiProperty({ description: 'Cantidad de perfumes en el pedido' })
+  cant: number;
+}
+
+export class UpdateOrderDto {
+  @ApiProperty({
+    description: 'Representa el estado del pedido',
+  })
+  @IsEnum(State)
+  state: State;
+  @ApiProperty({
+    description:
+      'Lista de perfumes en el pedido con sus respectivas cantidades',
+    type: [PerfumeOrderDto],
+  })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => PerfumeOrderDto)
+  perfumes: PerfumeOrderDto[];
+}
