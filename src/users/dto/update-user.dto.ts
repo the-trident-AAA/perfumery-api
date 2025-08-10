270c2f1518e9e8ff0c5beba7b3776c7111a8806e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-10 10:47:22 -0400

    feat(user): update UpdateUserDto to include API documentation properties for username and avatar, improving the clarity of the user model.

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,3 +1,17 @@
-import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateUserDto extends CreateUserDto {}
+export class UpdateUserDto {
+  @ApiProperty({
+    description: 'Representa el nombre de usuario del usuario',
+    type: 'string',
+    required: false,
+  })
+  username?: string;
+  @ApiProperty({
+    description: 'Representa el avatar del usuario',
+    type: 'string',
+    format: 'binary',
+    required: false,
+  })
+  avatar?: Express.Multer.File;
+}
