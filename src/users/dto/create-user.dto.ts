b2d317db2ca4b21f8b1c6284bfa35078c205319c
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-08-10 10:39:55 -0400

    feat(user): add documentation properties to CreateUserDto to improve API clarity, including username, email, password, and an optional avatar.

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Representa el nombre de usuario del usuario',
+    type: 'string',
+    required: true,
+  })
   username: string;
+  @ApiProperty({
+    description: 'Representa la dirección de correo electrónico del usuario',
+    type: 'string',
+    required: true,
+  })
   email: string;
+  @ApiProperty({
+    description: 'Representa la contraseña del usuario',
+    type: 'string',
+    required: true,
+  })
   password: string;
+  @ApiProperty({
+    description: 'Representa el avatar del usuario',
+    type: 'string',
+    format: 'binary',
+    required: false,
+  })
+  avatar?: Express.Multer.File;
 }
