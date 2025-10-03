1a8512693b85cb121afe2fee995f747af404a086
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-03 16:09:06 -0400

    feat(shop-cart): add merging of anonymous and user carts in ShopCartService

--- src/shop-cart/shop-cart.module.ts
+++ src/shop-cart/shop-cart.module.ts
@@ -5,9 +5,10 @@ import { ShopCartController } from './shop-cart.contoller';
 import { ShopCartPerfumeModule } from 'src/shop-cart-perfume/shop-cart-perfume.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShopCartEntity } from './entities/shop-cart.entity';
+import { PerfumeModule } from 'src/perfume/perfume.module';
 
 @Module({
-  imports: [DatabaseModule, ShopCartPerfumeModule, TypeOrmModule.forFeature([ShopCartEntity])],
+  imports: [DatabaseModule, ShopCartPerfumeModule, PerfumeModule, TypeOrmModule.forFeature([ShopCartEntity])],
   controllers: [ShopCartController],
   providers: [ShopCartService],
   exports: [ShopCartService],
--- src/shop-cart/shop-cart.service.ts
+++ src/shop-cart/shop-cart.service.ts
@@ -4,17 +4,105 @@ import { DatabaseService } from 'src/database/database.service';
 import { ShopCartResponse } from './responses/shop-cart.response';
 import { ShopCartPerfumeService } from 'src/shop-cart-perfume/shop-cart-perfume.service';
 import { ShopCartTotalItemsResponse } from './responses/shop-cart-total-items.response';
+import { PerfumeService } from 'src/perfume/perfume.service';
 
 @Injectable()
 export class ShopCartService {
   constructor(
     private readonly db: DatabaseService,
     private readonly shopCartPerfumeService: ShopCartPerfumeService,
+    private readonly perfumeService: PerfumeService,
   ) {}
 
   async create(createShopCartDto: CreateShopCartDto) {
     const shopCart = this.db.shopCartRespository.create(createShopCartDto);
     return await this.db.shopCartRespository.save(shopCart);
+  }
+
+  async fusionShopCart(sessionId: string, shopCartId: string) {
+    // find the anonymous shopcart
+    const anonymousShopCart = await this.db.shopCartRespository.findOne({
+      where: {
+        sessionId,
+      },
+      relations: ['shopCartPerfumes'],
+    });
+
+    if (!anonymousShopCart)
+      throw new BadRequestException(
+        'No existe un carrito anonimo para el session id especificado',
+      );
+
+    // find the user shopcart
+    const userShopCart = await this.db.shopCartRespository.findOne({
+      where: {
+        id: shopCartId,
+      },
+      relations: ['shopCartPerfumes'],
+    });
+
+    if (!userShopCart)
+      throw new BadRequestException(
+        'No existe el carrito de usuario especificado',
+      );
+
+    // Si no hay perfumes en el carrito anónimo, no hay nada que fusionar
+    if (
+      !anonymousShopCart.shopCartPerfumes ||
+      anonymousShopCart.shopCartPerfumes.length === 0
+    ) {
+      return userShopCart;
+    }
+
+    // Fusionar cada perfume del carrito anónimo al carrito del usuario
+    for (const anonymousPerfume of anonymousShopCart.shopCartPerfumes) {
+      // Buscar si el perfume ya existe en el carrito del usuario
+      const existingUserPerfume = userShopCart.shopCartPerfumes.find(
+        (up) => up.perfumeId === anonymousPerfume.perfumeId,
+      );
+
+      if (existingUserPerfume) {
+        // Si ya existe, sumar las cantidades
+        const totalQuantity = existingUserPerfume.cant + anonymousPerfume.cant;
+
+        // Verificar si hay suficiente stock disponible
+        const hasStock = await this.perfumeService.checkStocks(
+          anonymousPerfume.perfumeId,
+          totalQuantity,
+        );
+
+        if (hasStock) {
+          // Actualizar la cantidad en el carrito del usuario
+          existingUserPerfume.cant = totalQuantity;
+          await this.db.shopCartPerfumeRespository.save(existingUserPerfume);
+        } else {
+          // Si no hay suficiente stock, obtener la cantidad máxima disponible
+          const perfume = await this.db.perfumeRepository.findOne({
+            where: { id: anonymousPerfume.perfumeId },
+          });
+
+          if (perfume && perfume.cant > existingUserPerfume.cant) {
+            // Ajustar a la cantidad máxima disponible
+            existingUserPerfume.cant = perfume.cant;
+            await this.db.shopCartPerfumeRespository.save(existingUserPerfume);
+          }
+        }
+      } else {
+        // Crear nuevo perfume en el carrito del usuario
+        const newShopCartPerfume = this.db.shopCartPerfumeRespository.create({
+          perfumeId: anonymousPerfume.perfumeId,
+          shopCartId: shopCartId,
+          cant: anonymousPerfume.cant,
+        });
+        await this.db.shopCartPerfumeRespository.save(newShopCartPerfume);
+      }
+    }
+
+    // Limpiar el carrito anónimo después de la fusión
+    await this.remove(anonymousShopCart.id);
+
+    // Retornar el carrito del usuario actualizado
+    return await this.findOne(shopCartId);
   }
 
   async findOne(id: string) {
