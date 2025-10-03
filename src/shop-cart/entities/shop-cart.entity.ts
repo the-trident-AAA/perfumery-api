3a556a7d98cc5569517a376d0bad3d616414ce50
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-10-03 15:35:12 -0400

    feat(shop-cart): add optional sessionId field in ShopCartEntity to manage anonymous carts.

--- src/shop-cart/entities/shop-cart.entity.ts
+++ src/shop-cart/entities/shop-cart.entity.ts
@@ -1,10 +1,12 @@
 import { ShopCartPerfumeEntity } from 'src/shop-cart-perfume/entities/shop-cart-perfume.entity';
-import { Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, OneToMany, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity({ name: 'shop_cart' })
 export class ShopCartEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+  @Column({ name: 'session_id', nullable: true })
+  sessionId: string;
   @OneToMany(
     () => ShopCartPerfumeEntity,
     (shopCartPerfume) => shopCartPerfume.shopCart,
