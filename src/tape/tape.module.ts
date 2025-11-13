da570b33a258545f1e6a1cb674a038e884ed1f28
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-11-12 19:10:47 -0500

    feat(tape): import MinioModule into TapeModule for enhanced functionality

--- src/tape/tape.module.ts
+++ src/tape/tape.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TapeService } from './tape.service';
 import { TapeController } from './tape.controller';
+import { MinioModule } from 'src/minio/minio.module';
 
 @Module({
   controllers: [TapeController],
   providers: [TapeService],
+  imports: [MinioModule],
 })
 export class TapeModule {}
